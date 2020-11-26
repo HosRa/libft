59acc0ec427e4e4c46ad6f08e43ec4998f8cc9f3
thallard <thomas.hallard64@gmail.com>
2020-11-26 12:22:40 +0100

    ft_strtrim patched

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: thallard <thallard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/23 16:38:17 by thallard          #+#    #+#             */
-/*   Updated: 2020/11/24 15:49:50 by thallard         ###   ########lyon.fr   */
+/*   Updated: 2020/11/26 12:20:40 by thallard         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,5 @@ char	*ft_strtrim(char const *str, char const *set)
 	res = ft_substr((char *)str, 0, size + 1);
 	if (!res)
 		return (NULL);
-	if (res[0] == '\0')
-		return (ft_strdup(""));
 	return (res);
 }
